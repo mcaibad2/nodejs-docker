1b135ec78c92d43341fc64ba7bb9e15b80f3df03
Andreas Daskalopoulos <andreas.daskalopoulos@citrix.com>
2019-01-24 12:19:18 +0200

    Added winston logger.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const winston = require('winston');
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => {
+            return `${info.timestamp} ${info.level}: ${info.message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'falcon-app.log' })
+    ]
+});
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -5,8 +19,12 @@ const PORT = 3000;
 const DB_HOST = process.env.NODE_ENV === 'production' ? 'mongodb' : 'localhost';
 
 mongoose.connect(`mongodb://${DB_HOST}:27017/express-mongo`, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+    .then(() => {
+        logger.log('info', 'mongodb connected');
+    })
+    .catch(err => {
+        logger.log('error', err)
+    });
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
